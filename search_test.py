f9fd339764317497a64f5ac8c960cfd5fb8c4ec2
sm2401 <65030395+Suliko1979@users.noreply.github.com>
2021-04-21 15:28:13 +0600

    Update search_test.py

--- search_test.py
+++ search_test.py
@@ -1,1 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import pytest
+import random
+import random_words
+from random_words import RandomWords
 
+
+class TestAmazon:
+    rw = RandomWords()
+    search_words = rw.random_words(count=10)
+
+    def setup(self):
+        self.driver = webdriver.Chrome(executable_path='F:\django\Selenium lessons\chromedriver.exe')
+        self.driver.implicitly_wait(3)
+        self.driver.get('https://www.amazon.com/')
+
+    @pytest.mark.parametrize('search_query', search_words)
+    def test_search_amazon_dress(self, search_query):
+        search= self.driver.find_element_by_id("twotabsearchtextbox")
+        search.send_keys(search_query, Keys.ENTER)
+        expected = f'\"{search_query}\"'
+        actual = self.driver.find_element_by_xpath('/html/body/div[1]/div[2]/span/div/span/h1/div/div[1]/div/div'
+                                              '/span[3]').text
+        assert expected==actual, f'Error'
+        self.driver.close()
+
+    def closing_test(self):
+        self.driver.quit()
